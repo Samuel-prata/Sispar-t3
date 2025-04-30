d45b0c7e8e14c6cde67d2000f260ddbc3b53dc37
samuel-prata <samuel.silverio001@hotmail.com>
2025-04-30 19:28:34 -0300

    add: Aula 30/04 - Criacao do arquivo reembolso_model

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 # RESPONSAVEL POR CRIAR A APLICAÇÃO 
-
 from flask import Flask
 from src.controller.colaborador_controller import bp_colaborador
 from src.model import db
 from config import Config
+from flask_cors import CORS
 
 def create_app():
-    app = Flask(__name__) 
+    app = Flask(__name__) # <-- instancia do Flask
+    CORS(app, origins="*") # <---- A politica de CORS seja implementada em TODA A APLICAÇÃO 
     app.register_blueprint(bp_colaborador)
-    
     app.config.from_object(Config)
     
     db.init_app(app) # Inicia a conexão com o banco de dados
